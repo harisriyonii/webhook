5a7bbf70e02abd4cb45e6915924839cfb664c5b0
harisriyoni <1214047@std.ulbi.ac.id>
2024-09-21 18:38:13 +0700

    webhook

---
+++ type.go
@@ -0,0 +1,13 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"IHHH jangan SPAAM berisik tau aku lagi dijalan nih",
+	"Kamu ganteng tau",
+	"Ihhh kamu cantik banget",
+	"Senja memang indah tapi kamu tetap yang terindah Aseek",
+	"Anyong Aseoo Yeoboo, Aiigoo Haris lagi gaadaa",
+}
